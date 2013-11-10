69ccc15d850301a88c8aa4a15c26190cc0aeefcb
Spike Brehm <ocelot@gmail.com>
2013-11-10 14:47:17 -0800

    Use moment to format date

--- app/views/post.jsx
+++ app/views/post.jsx
@@ -1,16 +1,22 @@
 /**
  * @jsx React.DOM
  */
-var React = require('react-tools').React;
+var React = require('react-tools').React
+  , moment = require('moment')
+;
 
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <small>Posted by {this.props.author} at {this.props.created_at}</small>
+        <small>Posted by {this.props.author} at {formatDate(this.props.created_at)}</small>
         <p>{this.props.body}</p>
       </div>
     );
   }
 });
+
+function formatDate(dateStr) {
+  return moment(dateStr).format('LLLL');
+}
