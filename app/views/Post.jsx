2f5153073a6f7151c6ea0daf642d9d97edbd9c1c
Spike Brehm <ocelot@gmail.com>
2015-02-04 14:50:00 -0800

    Example showing moment

--- app/views/Post.jsx
+++ app/views/Post.jsx
@@ -1,17 +1,22 @@
 var React = require('react');
 var Renderer = require('./Renderer.jsx');
+var moment = require('moment');
 
 var Post = React.createClass({
   render: function() {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <small>Posted by {this.props.author} at {this.props.created_at}</small>
+        <small>Posted by {this.props.author} at {formatDate(this.props.created_at)}</small>
         <p>{this.props.body}</p>
         <Renderer renderer={this.props.renderer} />
       </div>
     );
   }
 });
+
+function formatDate(dateStr) {
+  return moment(dateStr).format('LLLL');
+}
 
 module.exports = Post;
