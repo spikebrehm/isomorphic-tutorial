ea83adeab84fd67f8ae34e9fe95cd9666367abe1
Spike Brehm <ocelot@gmail.com>
2015-02-05 14:39:13 -0800

    forgot to push middleware

---
+++ app/router/middleware.js
@@ -0,0 +1,23 @@
+/*
+ * Express middleware function, for mounting routes onto an Express app.
+ */
+module.exports = function middleware(router) {
+  var directorRouter = router.directorRouter;
+
+  return function middleware(req, res, next) {
+    // Attach `this.next` to route handler, for better handling of errors.
+    directorRouter.attach(function() {
+      this.next = next;
+    });
+
+    // Dispatch the request to the Director router.
+    directorRouter.dispatch(req, res, function (err) {
+      // When a 404, just forward on to next Express middleware.
+      if (err && err.status === 404) {
+        next();
+      }
+    });
+  };
+};
+
+
